46bb1526aa1b4cc2bcfc3ec61bf87a2392ac3ee8
DzmitryU <dzmitry.urbanovich@arteza.com>
2022-05-14 17:39:22 +0300

    updated library works with php 8.0

--- examples/env.php
+++ examples/env.php
@@ -1,6 +1,6 @@
 <?php
 
-const MERCHANT_ID = 'merchant_fraOKFMaGqymr5GwMddgh';
-const SECRET_KEY = 'gpKey_6xHv6sBeaRceqbXrWepxug';
+const MERCHANT_ID = 'merchant_sidERBMaGqymr5GwMddgh';
+const SECRET_KEY = 'gpKey_160uV5hgud3mXq9IH9nNu9';
 const IS_LIVE = false;
 const IS_US = false;
